3078032a568ed602d2f9c157e167a96f1a643590
dophu17 <dophu17@gmail.com>
2016-10-20 17:51:29 +0700

    mail

--- campaign/index.php
+++ campaign/index.php
@@ -6,10 +6,12 @@
  */
 
 require '../libs/Smarty.class.php';
+require '../libs/PHPMailer/PHPMailerAutoload.php';
 include '../configs/config.php';
 include 'input.php';
 
 $smarty = new Smarty;
+$mailer = new PHPMailer;
 $input = new Input;
 session_start();
 
@@ -60,21 +62,45 @@ if ( isset($_POST['send']) ) {
 	$from_name = mb_encode_mimeheader(mb_convert_encoding(FROM_NAME,'iso-2022-jp','utf-8'));
 	$headers = "From:" . $from_name . '<' . FROM_EMAIL . '>';
 
-	ini_set("SMTP","smtp.gmail.com");
-	ini_set("smtp_port", 587);
+	// ini_set("SMTP","smtp.gmail.com");
+	// ini_set("smtp_port", 587);
 	// ini_set("username","dophu17@gmail.com");
 	// ini_set("password","kcrtftpisnatcjyt ");
-	ini_set("sendmail_from","dophu17@gmail.com");
+	// ini_set("sendmail_from","dophu17@gmail.com");
 
-	if ( !mb_send_mail($_POST['mail'], TITLE_CONTACT_GUEST, $body, $headers) ) {
-		echo "mb_send_mail() failed.\n";
-		return $smarty->display('confirm.tpl');
-	}
+	// if ( !mb_send_mail($_POST['mail'], TITLE_CONTACT_GUEST, $body, $headers) ) {
+	// 	echo "mb_send_mail() failed.\n";
+	// 	return $smarty->display('confirm.tpl');
+	// }
 
 	// if ( !mb_send_mail(MANAGE_CAMPAIGN_EMAIL, $subject, $body, $headers) ) {
 	//     echo "mb_send_mail() failed.\n";
 	//     return false;
 	// }
+
+
+	$mailer->isSMTP();                                      // Set mailer to use SMTP
+	$mailer->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+	$mailer->SMTPAuth = true;                               // Enable SMTP authentication
+	$mailer->Username = 'dophu17@gmail.com';                 // SMTP username
+	$mailer->Password = 'kcrtftpisnatcjyt';                           // SMTP password
+	$mailer->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	$mailer->Port = 587;                                    // TCP port to connect to
+
+	$mailer->setFrom(FROM_EMAIL, FROM_NAME);
+	$mailer->addAddress($_POST['mail'], $_POST['name']);
+	$mailer->isHTML(false);                                  // Set email format to HTML
+
+	$mailer->Subject = TITLE_CONTACT_GUEST;
+	$mailer->Body    = $smarty->fetch('mail_manage.tpl');
+
+	if( !$mailer->send() ) {
+	    echo 'Mailer Error: ' . $mailer->ErrorInfo;
+	    return false;
+	} else {
+	    echo 'Message has been sent';
+	}
+
 
 	return $smarty->display('send.tpl');
 }
